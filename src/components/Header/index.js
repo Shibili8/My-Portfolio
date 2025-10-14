a7cb74cc902e2cc02aeadc288d6742b7e6cd8539
Shibili8 <shibilishibizz99@gmail.com>
2025-10-14 14:46:25 +0530

    Updated the syles for mobile view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,51 +1,67 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react"
+import {Link} from "react-router-dom"
+import { GiHamburgerMenu } from "react-icons/gi"
 
-import './index.css';
+import './index.css'
 
-const Header = () => {
-    const [isOpen, setOpen] = useState(false);
+const Header = () =>{
+    const [isOpen, setOpen] = useState(false)
 
-    const onClickHamburger = () => {
-        setOpen(prev => !prev);
+    const onClickHamburger = () =>{
+        setOpen(prev=>!prev)
     }
 
     return (
         <>
-            <div className="header-container">
-                <Link to="/" className="link-style">
-                    <h3 className="header-name">Mohammed Shibili K</h3>
-                </Link>
-
-                <div className="header-options-container">
-                    <div className="hamburger-wrapper">
-                        <button className="hamburger-btn" type="button" onClick={onClickHamburger}>
-                            <GiHamburgerMenu size={24} color="#f2f2f2"/>
-                        </button>
-
-                        {isOpen && (
-                            <div className="header-options-mobile-device">
-                                <Link to="/" className="link-style"><h3 className="header-options">Home</h3></Link>
-                                <Link to="/education" className="link-style"><h3 className="header-options">Education</h3></Link>
-                                <Link to="/projects" className="link-style"><h3 className="header-options">Projects</h3></Link>
-                                <Link to="/certificates" className="link-style"><h3 className="header-options">Certificates</h3></Link>
-                                <Link to="/contact" className="link-style"><h3 className="header-options">Contact</h3></Link>
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="header-options-larger-device">
-                        <Link to="/" className="link-style"><h3 className="header-options">Home</h3></Link>
-                        <Link to="/education" className="link-style"><h3 className="header-options">Education</h3></Link>
-                        <Link to="/projects" className="link-style"><h3 className="header-options">Projects</h3></Link>
-                        <Link to="/certificates" className="link-style"><h3 className="header-options">Certificates</h3></Link>
-                        <Link to="/contact" className="link-style"><h3 className="header-options">Contact</h3></Link>
-                    </div>
-                </div>
+        <div className="header-container">
+            <Link to="/" className="link-style">
+                <h3 className="header-name">Mohammed Shibili K</h3>
+            </Link>
+            <div className="header-options-container">
+                <button className="hamburger-btn" type="button" onClick={onClickHamburger} >
+                    <GiHamburgerMenu size={20} color="#f2f2f2"/>
+                </button>
+                <div className="header-options-larger-device">
+                    <Link to="/" className="link-style">
+                        <h3 className="header-options">Home</h3>
+                    </Link>
+                    <Link to="/education" className="link-style">
+                        <h3 className="header-options">Education</h3>
+                    </Link>
+                    <Link to="/projects" className="link-style">
+                        <h3 className="header-options">Projects</h3>
+                    </Link>
+                    <Link to="/certificates" className="link-style">
+                        <h3 className="header-options">Certificates</h3>
+                    </Link>
+                    <Link to="/contact" className="link-style">
+                        <h3 className="header-options">Contact</h3>
+                    </Link>
+                </div>  
             </div>
+            
+        </div>
+        { isOpen ? (
+                    <div className="header-options-mobile-device">
+                        <Link to="/" className="link-style">
+                            <h3 className="header-options">Home</h3>
+                        </Link>
+                        <Link to="/education" className="link-style">
+                            <h3 className="header-options">Education</h3>
+                        </Link>
+                        <Link to="/projects" className="link-style">
+                            <h3 className="header-options">Projects</h3>
+                        </Link>
+                        <Link to="/certificates" className="link-style">
+                            <h3 className="header-options">Certificates</h3>
+                        </Link>
+                        <Link to="/contact" className="link-style">
+                            <h3 className="header-options">Contact</h3>
+                        </Link>
+                    </div>
+                ): null}
         </>
-    );
+    )
 }
 
-export default Header;
+export default Header
